2c8d273928b40866bbec8ba560113cb8f46818fb
MeineZ <meinezeinstra@gmail.com>
2022-12-31 14:17:28 +0100

    Initial project setup

---
+++ DemoTouchRatio/DemoTouchRatio/DemoTouchRatio.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "DemoTouchRatio.h"
+
+
+BAKKESMOD_PLUGIN(DemoTouchRatio, "Keeps track of your ratio between balltouches, car bumps, and demos.", plugin_version, 0)
+
+std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
+
+void DemoTouchRatio::onLoad()
+{
+	_globalCvarManager = cvarManager;
+	//LOG("Plugin loaded!");
+	// !! Enable debug logging by setting DEBUG_LOG = true in logging.h !!
+	//DEBUGLOG("DemoTouchRatio debug mode enabled");
+
+	// LOG and DEBUGLOG use fmt format strings https://fmt.dev/latest/index.html
+	//DEBUGLOG("1 = {}, 2 = {}, pi = {}, false != {}", "one", 2, 3.14, true);
+
+	//cvarManager->registerNotifier("my_aweseome_notifier", [&](std::vector<std::string> args) {
+	//	LOG("Hello notifier!");
+	//}, "", 0);
+
+	//auto cvar = cvarManager->registerCvar("template_cvar", "hello-cvar", "just a example of a cvar");
+	//auto cvar2 = cvarManager->registerCvar("template_cvar2", "0", "just a example of a cvar with more settings", true, true, -10, true, 10 );
+
+	//cvar.addOnValueChanged([this](std::string cvarName, CVarWrapper newCvar) {
+	//	LOG("the cvar with name: {} changed", cvarName);
+	//	LOG("the new value is: {}", newCvar.getStringValue());
+	//});
+
+	//cvar2.addOnValueChanged(std::bind(&DemoTouchRatio::YourPluginMethod, this, _1, _2));
+
+	// enabled decleared in the header
+	//enabled = std::make_shared<bool>(false);
+	//cvarManager->registerCvar("TEMPLATE_Enabled", "0", "Enable the TEMPLATE plugin", true, true, 0, true, 1).bindTo(enabled);
+
+	//cvarManager->registerNotifier("NOTIFIER", [this](std::vector<std::string> params){FUNCTION();}, "DESCRIPTION", PERMISSION_ALL);
+	//cvarManager->registerCvar("CVAR", "DEFAULTVALUE", "DESCRIPTION", true, true, MINVAL, true, MAXVAL);//.bindTo(CVARVARIABLE);
+	//gameWrapper->HookEvent("FUNCTIONNAME", std::bind(&TEMPLATE::FUNCTION, this));
+	//gameWrapper->HookEventWithCallerPost<ActorWrapper>("FUNCTIONNAME", std::bind(&DemoTouchRatio::FUNCTION, this, _1, _2, _3));
+	//gameWrapper->RegisterDrawable(bind(&TEMPLATE::Render, this, std::placeholders::_1));
+
+
+	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", [this](std::string eventName) {
+	//	LOG("Your hook got called and the ball went POOF");
+	//});
+	// You could also use std::bind here
+	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", std::bind(&DemoTouchRatio::YourPluginMethod, this);
+}
